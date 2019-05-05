212d22a0763ccb4b67c8bf33c954d1cb85b93b6b
Adeel Pervaiz <>
2019-05-06 00:02:42 +0500

    fixed server

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -7,6 +7,8 @@ use stdClass;
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+require_once 'Format.php';
+
 /**
  * CodeIgniter Rest Controller
  * A fully RESTful server implementation for CodeIgniter using one library, one config file and one controller.
